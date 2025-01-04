b5b9787b5e9739685c83847f574e00f0ecd3ff93
smivys <khanhnamxz@hotmail.com>
2025-01-04 06:59:44 +0100

    redefine the dataset

--- src/downloader.py
+++ src/downloader.py
@@ -1,17 +1,16 @@
-# import argparse
-import Bio
+from Bio import Entrez
 
 #@title FUNCTION: DOWNLOAD_GENOME(organism)
 def download_genome(organism_name):
   try:
     # Step 1: Search for the organism's genome in the NCBI nucleotide database
     print(f"Searching for complete genome of {organism_name}...")
-    search_handle = Bio.Entrez.esearch(
+    search_handle = Entrez.esearch(
         db="nucleotide",  # Search the nucleotide database
         term=f"{organism_name}[Organism] AND complete genome[Title]",
         retmax=1  # Retrieve only the top result
     )
-    search_results = Bio.Entrez.read(search_handle)
+    search_results = Entrez.read(search_handle)
     search_handle.close()
 
     # Get the first GenBank ID (if found)
@@ -24,19 +23,17 @@ def download_genome(organism_name):
 
     # Step 2: Fetch the genome data in FASTA format
     print("Downloading genome in FASTA format...")
-    fetch_handle = Bio.Entrez.efetch(
+    fetch_handle = Entrez.efetch(
         db="nucleotide",
         id=genome_id,
         rettype="fasta",
         retmode="text"
     )
-    genome_seq = fetch_handle.read()
+    genome_seq = fetch_handle.read().split("\n", 1)[1].replace("\n", "")
     fetch_handle.close()
     print(f"Genome is successfully downloaded.")
 
-    return organism_name, genome_seq
+    return genome_seq
 
   except Exception as e:
     print(f"An error occurred: {e}")
-
-
